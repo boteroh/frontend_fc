0435cb7ac9f0e3563aa54354eedc5387b858b813
Alejandro Botero <a.b.hincapie@gamail.com>
2024-09-17 09:05:31 -0500

    Create and agregate users, collection, images and script files

---
+++ scripts/modal-control.js
@@ -0,0 +1,10 @@
+// Control all modals
+function openModal() {
+    document.getElementById('modal').classList.remove('hidden');
+    document.getElementById('modal').classList.add('flex');
+}
+
+function closeModal() {
+    document.getElementById('modal').classList.add('hidden');
+    document.getElementById('modal').classList.remove('flex');
+}
---
+++ scripts/users.js
@@ -0,0 +1,1 @@
+// Execute modal new user
