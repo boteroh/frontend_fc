f2974ad90b1a8356a66bde6a13db22c398967ad9
Alejandro Botero <a.b.hincapie@gamail.com>
2024-09-19 17:33:33 -0500

    add update user function

--- scripts/modal-control.js
+++ scripts/modal-control.js
@@ -1,9 +1,55 @@
 // Control all modals
-function openModal() {
+// function openModal() {
+//     document.getElementById('modal').classList.remove('hidden');
+//     document.getElementById('modal').classList.add('flex');
+// }
+
+// function closeModal() {
+//     document.getElementById('modal').classList.add('hidden');
+//     document.getElementById('modal').classList.remove('flex');
+// }
+
+// Función para abrir modal para crear o actualizar usuario
+function openModal(user = null) {
+    const modalTitle = document.querySelector('#modal h1');
+    const saveButton = document.querySelector('#modal button[type="submit"]');
+
+    if (user) {
+        // Si se pasa un usuario, es para actualizar
+        modalTitle.textContent = "EDIT USER";
+        saveButton.textContent = "Update User";
+
+        // Rellenar los campos del formulario con los datos del usuario
+        document.getElementById('names').value = user.names;
+        document.getElementById('lastname').value = user.lastName;
+        document.getElementById('document').value = user.documentId; // Si tienes este campo
+        document.getElementById('role').value = user.role;
+        document.getElementById('area').value = user.area;
+        document.getElementById('username').value = user.userName;
+        // Passwords empty
+        document.getElementById('password').value = "";
+        document.getElementById('confirm-password').value = "";
+
+        // Cambia la función del botón para que sea de actualización
+        saveButton.setAttribute('onclick', 'updateUser()');
+    } else {
+        // Si no se pasa un usuario, es para crear
+        modalTitle.textContent = "NEW USER";
+        saveButton.textContent = "Save User";
+
+        // Limpiar los campos del formulario
+        document.getElementById('newUserForm').reset();
+
+        // Cambia la función del botón para que sea de creación
+        saveButton.setAttribute('onclick', 'createUser()');
+    }
+
+    // Mostrar el modal
     document.getElementById('modal').classList.remove('hidden');
     document.getElementById('modal').classList.add('flex');
 }
 
+// Función para cerrar modal
 function closeModal() {
     document.getElementById('modal').classList.add('hidden');
     document.getElementById('modal').classList.remove('flex');
--- scripts/users.js
+++ scripts/users.js
@@ -32,31 +32,41 @@ const listUsers = async () => {
 
       // Iterate through the users and generate the table rows
       list.map(function(user) {
+        console.log('user object', user);
+
         tableContent += `
             <tr>
               <td class="p-2.5">${user.documentId}</td>
               <td class="p-2.5">${user.names}</td>
               <td class="p-2.5">${user.area}</td>
               <td class="p-2.5">${user.role}</td>
-              <td class="p-2.5">${user.username}</td>
+              <td class="p-2.5">${user.userName ? user.userName : 'No username available'}</td>
               <td class="p-2.5">
-                <button class="mx-1">
-                  <img src="/img/icons8-ver-24.png" alt="Visualizar" class="h-4 w-4">
+                <button class="mx-1 view-btn" data-id="${user.documentId}"">
+                  <img src="/img/icons8-ver-24.png" alt="View" class="h-4 w-4">
                 </button>
-                <button class="mx-1">
-                  <img src="/img/icons8-editar.svg" alt="Editar" class="h-4 w-4">
+                <button class="mx-1 edit-btn" data-id="${user.documentId}">
+                  <img src="/img/icons8-editar.svg" alt="Edit" class="h-4 w-4">
                 </button>
-                <button class="mx-1">
-                  <img src="/img/icons8-eliminar.svg" alt="Eliminar" class="h-4 w-4">
+                <button class="mx-1 delete-btn" data-id="${user.documentId}">
+                  <img src="/img/icons8-eliminar.svg" alt="delete" class="h-4 w-4">
                 </button>
               </td>
             </tr>`;
       });
 
-      console.log(list);
-
       // Add content to table
       tableBody.innerHTML = tableContent;
+
+      // Add event listeners to the edit buttons after the table has been populated
+      const editButtons = document.querySelectorAll( '.edit-btn' );
+      editButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+          const userID = event.currentTarget.getAttribute( 'data-id' );
+          updateUser(userID);
+        });
+      });
+
     } else {
       // Control if not found users
       tableBody.innerHTML = "<tr><td colspan='5'>No hay usuarios disponibles</td></tr>";
@@ -71,318 +81,72 @@ const listUsers = async () => {
 
 document.addEventListener('DOMContentLoaded', listUsers);
 
-
 //---------------------------------------------------------------------------------------------------------------------------//
-
-const createUser = async () => {
-  // Obtener los valores de los campos del formulario directamente
-  const user = {
-      names: document.getElementById('names').value,
-      lastname: document.getElementById('lastname').value,
-      documentId: document.getElementById('document').value,
-      role: document.getElementById('role').value,
-      // username: document.getElementById('username').value,
-      password: document.getElementById('password').value,
-      confirmpassword: document.getElementById('confirm-password').value,
-      // Obtener los valores de los checkboxes
-      area: Array.from(document.querySelectorAll('input[name^="area"]:checked')).map(cb => cb.nextSibling.textContent.trim())
-  };
-
-  // Validar que las contraseñas coinciden
-  if (user.password !== user.confirmpassword) {
-      alert('The passwords do not match.');
-      return;
-  }
-
-  try {
-      // Realizar la petición fetch
-      const response = await fetch('http://localhost:3000/users/create-user', {
-          method: 'POST',
-          headers: {"Content-Type": "application/json; charset=UTF-8"},
-          body: JSON.stringify(user)
-      });
-
-      // Obtener la respuesta como JSON
-      const result = await response.json();
-
-      if (response.ok) {
-          alert(result.msg);
-          closeModal();
-          listUsers();
-      } else {
-          console.error('Error adding user:', result.message);
-          alert('Error adding user.');
-      }
-  } catch (error) {
-      console.error('Error adding user:', error);
-      alert('Error adding user');
-  }
-};
-
-// Añadir el event listener al formulario
-document.querySelector('#newUserForm').addEventListener('submit', function(event) {
-  event.preventDefault();
-  createUser();
-});
-
 
 //-------------------------------//
 // <-----> CREATE NEW USER <---->//
 //-------------------------------//
-// const createUser = async () => {
-//   const url = "http://localhost:3000/users/create-user";
-//   const newUserForm = document.querySelector( '#newUserForm form' );
+const createUser = async () => {
+  const url = "http://localhost:3000/users/create-user"
 
-//   // Function to handle form submission
-//   newUserForm.addEventListener( 'submit', function(event) {
-//     event.preventDefault();
+  const names = document.getElementById('names').value;
+  const lastName = document.getElementById('lastname').value;
+  const userName = `${names}.${lastName}` 
+  document.getElementById('username').value = userName.toLowerCase();
+  
+  const user = {
+    documentId: document.getElementById('document').value,
+    names: names,
+    lastName: lastName,
+    area: document.getElementById('area').value,
+    role: document.getElementById('role').value,
+    userName: userName,
+    password: document.getElementById('password').value,
+    confirmpassword: document.getElementById('confirm-password').value,
+  }
+  // alert(user.username); // Verifica qué dato está devolviendo el objeto
+  fetch(url, {
+    method: 'POST',
+    momde: 'cors',
+    body: JSON.stringify(user),
+    headers: { 'Content-type': 'application/json; charset=UTF-8' }
+  })
+  .then((resp) => resp.json())
+  .then(json => {
+    alert(json.msg)
+  })
+};
 
-//     // Get the form data
-//     const userData = new FormData(newUserForm);
-//     const data = {
-//       names: userData.get( 'names' ),
-//       lastname: userData.get( 'lastname' ),
-//       documentId: userData.get( 'document' ),
-//       role: userData.get( 'role' ),
-//       username: userData.get( 'lastname' ),
-//       password: userData.get( 'password' ),
-//       confirmpassword: userData.get( 'confirm-password' )
-//     };
+// Update input Username
+document.getElementById('lastname').addEventListener('input', () => {
+  const names = document.getElementById('names').value;
+  const lastName = document.getElementById('lastname').value;
+  
+  // Concat and update input username
+  const userName = `${names}.${lastName}`; 
+  document.getElementById('username').value = userName.toLowerCase();
+});
 
-//     // VALIDATE PASSWORD
-//     if( data.password !== data.confirmpassword ) {
-//       alert( 'The passwords do not match.' );
-//       return;
-//     }
-//   });
-
-//   fetch(url, {
-//     method: 'POST',
-//     headers: { "Content-type": "application/json; charset=UTF-8" },
-//     body: JSON.stringify(data)
-//   })
-//   .then( response => response.json() )
-//   .then( result => {
-//     if ( result.success ) {
-//       closeModal();
-//       listUsers();
-//     } else {
-//       console.error( 'Error adding user:', result.message );
-//       alert( 'Error adding usaer.' );
-//     }
-//   })
-//   .catch( error => {
-//     console.error( 'Error adding user:', error );
-//     alert( 'Error adding user' );
-//   });
-//   listUsers();
-
-// };
-
-// const createUser = async () => {
-//   const url = "http://localhost:3000/users/create-user";
-//   const newUserForm = document.querySelector('#newUserForm');
-
-//   if (!newUserForm) {
-//     console.error('Form not found');
-//     return;
-//   }
-
-//   // Handle form submission
-//   newUserForm.addEventListener('submit', function (event) {
-//     event.preventDefault();
-
-//     // Get form data
-//     const userData = new FormData(newUserForm);
-//     const data = {
-//       names: userData.get('names'),
-//       lastName: userData.get('lastname'),
-//       documentId: userData.get('document'),
-//       role: userData.get('role'),
-//       username: `${userData.get('names')}.${userData.get('lastname')}`,
-//       password: userData.get('password'),
-//       confirmpassword: userData.get('confirm-password')
-//     };
-
-//     // Validate passwords match
-//     if (data.password !== data.confirmpassword) {
-//       alert('The passwords do not match.');
-//       return;
-//     }
-
-//     // Make the POST request
-//     fetch(url, {
-//       method: 'POST',
-//       headers: { "Content-type": "application/json; charset=UTF-8" },
-//       body: JSON.stringify(data)
-//     })
-
-//     .then(response => {
-//       console.log(response);  // Verifica el estado de la respuesta
-//       if (!response.ok) {
-//         throw new Error(`HTTP error! status: ${response.status}`);
-//       }
-//       return response.json();
-//     })
-
-//     .then(result => {
-//       if (result.success) {
-//         closeModal();
-//         listUsers();
-//       } else {
-//         console.error('Error adding user:', result.message);
-//         alert('Error adding user.');
-//       }
-//     })
-//     .catch(error => {
-//       console.error('Error adding user:', error);
-//       alert('Error adding user');
-//     });
-//   });
-// };
-
-// createUser();
-
-// const createUser = () => {
-//   const url = "http://localhost:3000/users/create-user";
-//   const newUserForm = document.querySelector('#newUserForm');
-
-//     // Función para abrir la modal
-//     // function openModal() {
-//     //   document.getElementById('modal').classList.remove('hidden');
-//     // }
-
-//     // Función para cerrar la modal
-//     function closeModal() {
-//         document.getElementById('modal').classList.add('hidden');
-//     }
-
-//   if (!newUserForm) {
-//     console.error('Form not found');
-//     return;
-//   }
-
-//   // Handle form submission
-//   newUserForm.addEventListener('submit', async function (event) {
-//     event.preventDefault();
-    
-//     // Get form data
-//     const userData = new FormData(newUserForm);
-//     const data = {
-//       names: userData.get('names'),
-//       lastName: userData.get('lastname'),
-//       documentId: userData.get('document'),
-//       role: userData.get('role'),
-//       // username: `${userData.get('names')}.${userData.get('lastname')}`,
-//       password: userData.get('password'),
-//       confirmpassword: userData.get('confirm-password')
-//     };
-
-//     // Validate passwords match
-//     if (data.password !== data.confirmpassword) {
-//       alert('Las contraseñas no coinciden.');
-//       return;
-//     }
-
-//     try {
-//       // Make the POST request
-//       const response = await fetch(url, {
-//         method: 'POST',
-//         headers: { "Content-type": "application/json; charset=UTF-8" },
-//         body: JSON.stringify(data)
-//       });
-
-//       console.log('Response status:', response.status);
-//       console.log('Response headers:', response.headers);
-
-//       if (!response.ok) {
-//         throw new Error(`HTTP error! status: ${response.status}`);
-//       }
-
-//       const result = await response.json();
-//       console.log('Response body:', result);
-
-//       if (result.success) {
-//         alert('Usuario añadido exitosamente.');
-//         // Asegúrate de que estas funciones estén definidas
-//         if (typeof closeModal === 'function') closeModal();
-//         if (typeof listUsers === 'function') listUsers();
-//       } else {
-//         throw new Error(result.message || 'Error al añadir usuario.');
-//       }
-//     } catch (error) {
-//       console.error('Error al añadir usuario:', error);
-//       alert(`Error al añadir usuario: ${error.message}`);
-//     }
-//   });
-// };
-
-// createUser();
-
-// const createUser = async () => {
-//   const url = "http://localhost:3000/users/create-user";
-//   const newUserForm = document.querySelector('#newUserForm');
-
-//   // Verificar si el formulario existe
-//   if (!newUserForm) {
-//     console.error('Form not found');  // Si no encuentra el formulario, muestra este mensaje
-//     return;
-//   } else {
-//     console.log('Form found');  // Si lo encuentra, muestra este mensaje
-//   }
-
-//   newUserForm.addEventListener('submit', function(event) {
-//     event.preventDefault();
-
-//     const userData = new FormData(newUserForm);
-
-//     const areas = [];
-//     const areaInputs = ['area1', 'area2', 'area3', 'area4', 'area5', 'area6'];
-
-//     areaInputs.forEach(areaId => {
-//       const areaCheckbox = document.getElementById(areaId);
-//       if (areaCheckbox && areaCheckbox.checked) {
-//         areas.push(areaCheckbox.name);  // Or areaCheckbox.value
-//       }
-//     });
-
-//     const data = {
-//       names: userData.get('names'),
-//       lastName: userData.get('lastname'),
-//       documentId: userData.get('document'),
-//       role: userData.get('role'),
-//       area: areas,
-//       password: userData.get('password'),
-//       confirmpassword: userData.get('confirm-password')
-//     };
-
-//     // Log the data before sending
-//     console.log('Data to be sent:', data);
-
-//     if (data.password !== data.confirmpassword) {
-//       alert('The passwords do not match.');
-//       return;
-//     }
-
-//     fetch(url, {
-//       method: 'POST',
-//       headers: { "Content-type": "application/json; charset=UTF-8" },
-//       body: JSON.stringify(data)
-//     })
-//     .then(response => response.json())
-//     .then(result => {
-//       if (result.msg === "User created succesfully") {
-//         closeModal();
-//         listUsers();
-//       } else {
-//         alert('Error adding user: ' + result.msg);
-//       }
-//     })
-//     .catch(error => {
-//       console.error('Error adding user:', error);
-//       alert('Error adding user');
-//     });
-//   });
-// };
-
-
+//-------------------------------//
+// <-------> UPDATE USER <------>//
+//-------------------------------//
+const updateUser = async() => {
+  const url = "http://localhost:3000/users/update"
+  const user = {
+    names: document.getElementById( 'names' ).value,
+    lastName: document.getElementById( 'lastname' ).value,
+    area: document.getElementById( 'area' ).value,
+    role: document.getElementById( 'role' ).value,
+    userName: document.getElementById( 'username' ).value,
+  }
+  fetch(url, {
+    method: 'PUT',
+    momde: 'cors',
+    body: JSON.stringify(user),
+    headers: { 'Content-type': 'application/json; charset=UTF-8' }
+  })
+  .then((resp) => resp.json())
+  .then(json => {
+    alert(json.msg)
+  });
+};
